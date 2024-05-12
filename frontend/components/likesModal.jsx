335c7cf97d84931fc5c2768ddba25b725d951346
Darius <darius.vincent5@gmail.com>
2024-05-12 21:59:56 +0800

    Improved Likes Bottom Sheet

--- frontend/components/likesModal.jsx
+++ frontend/components/likesModal.jsx
@@ -1,26 +1,30 @@
-import { View, ScrollView, Text, Modal, Button, Image, ImageBackground } from 'react-native';
+import { View, ScrollView, Text, ImageBackground } from 'react-native';
 import React from 'react';
 import { profileLikes } from '../constants/profiles';
+import { Shadow } from 'react-native-shadow-2';
 
 const LikesModal = () => {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} className="w-full h-full">
       <View className="justify-start items-start h-full w-full">
 
-        <View className="justify-start items-start w-full ml-2 mb-3">
+        <View className="justify-start items-start w-full ml-2">
           <Text className="font-outfitbold text-black text-2xl">
             Profiles that liked you!
           </Text>
         </View>
 
-        <View className="flex-1 flex-row items-center justify-around flex-wrap my-2">
+        <View className="flex-1 flex-row items-center justify-around flex-wrap my-5">
           {
             profileLikes.map((profile, index) => (
               <View
-                className="w-fit h-fit"
-                key={index}>
+              className="p-1"
+              key={index}>
+              <Shadow
+                className="rounded-2xl bg-transparent"
+                distance={10}>
                 <ImageBackground 
-                  className="rounded-2xl overflow-hidden mx-0 my-1 w-36 h-36 justify-end"
+                  className="rounded-2xl overflow-hidden w-36 h-36 justify-end"
                   source={profile.image}
                   resizeMode='cover'
                 >
@@ -33,6 +37,7 @@ const LikesModal = () => {
                     </Text>
                   </View>
                 </ImageBackground>
+              </Shadow>
               </View>
             ))
           }
